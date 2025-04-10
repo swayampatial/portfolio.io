641ae8ac7782e717d553189ea3bd6049cbe4ed55
swayam patial <swayampatial1@gmail.com>
2025-04-10 14:35:06 +0530

    first commit

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('fade-in');
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.1 });
+
+document.querySelectorAll('section').forEach((section) => {
+  observer.observe(section);
+});
